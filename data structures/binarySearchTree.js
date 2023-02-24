a27ed9de9dcc6900c55c78411dd76f13c902bd45
Adam Harte <adamh@CA-MBP-458.local>
2023-02-24 12:04:43 +0000

    binary search tree data structure added

---
+++ data structures/binarySearchTree.js
@@ -0,0 +1,127 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val) {
+    const newNode = new Node(val);
+
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+
+      while (true) {
+        if (val === current.val) return undefined;
+
+        if (val < current.vall) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (val > current.val) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+
+  find(val) {
+    if (this.root === null) return false;
+
+    let current = this.root;
+    let found = false;
+
+    while (current && !found) {
+      if (val < current.val) {
+        current = current.left;
+      } else if (val > current.val) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  BFS() {
+    let node = this.root;
+    const data = [];
+    const queue = [];
+
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.val);
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+
+    return data;
+  }
+
+  DFSPreOrder() {
+    const data = [];
+
+    function traverse(node) {
+      data.push(node.val);
+
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+
+    return data;
+  }
+
+  DFSPostOrder() {
+    const data = [];
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+
+      data.push(node.val);
+    }
+
+    traverse(this.root);
+
+    return data;
+  }
+
+  DFSInOrder() {
+    const data = [];
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+
+      data.push(node.val);
+
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+
+    return data;
+  }
+}
